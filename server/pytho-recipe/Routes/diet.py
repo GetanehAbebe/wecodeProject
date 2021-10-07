a2148d176ac2285917d717ef04b49d81ffeca933
GetanehAbebe <82867396+GetanehAbebe@users.noreply.github.com>
2021-10-07 12:10:19 +0300

    Delete server/pytho-recipe directory

--- server/pytho-recipe/Routes/diet.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, Blueprint
-from controllers.diets import get_diets
-
-diets = Blueprint('diets', __name__, url_prefix='/diets')
-
-
-@diets.route('/', methods=['GET'])
-def diets_name():
-    return get_diets()
