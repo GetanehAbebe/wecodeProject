2a548a85853c55cf189420d63396dfca21e54c33
GetanehAbebe <82867396+GetanehAbebe@users.noreply.github.com>
2021-10-07 12:10:44 +0300

    Delete server/recipesserver directory

--- server/recipesserver/routes/index.js
+++
@@ -1,4 +0,0 @@
-var express = require("express");
-var router = express.Router();
-const multer = require('multer');
-module.exports = router;
--- server/recipesserver/utill/database.js
+++
@@ -1,7 +0,0 @@
-const Sequelize = require("sequelize");
-
-const sequelize = new Sequelize('wecodepoject2', 'root', '123getaneh', {
-    dialect: 'mysql',
-    host: 'localhost'
-})
-module.exports = sequelize;
